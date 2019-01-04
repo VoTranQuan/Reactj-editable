5df2584ce41f1b857de54341271192305032eab1
quanzero <vtquan243@gmail.com>
2019-01-05 01:27:22 +0700

    modify UI

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 ## Install Scripts
 
-`npm install`
+### `npm install`
+
 then install immutability-helper
-`npm i immutability-helper`
+
+### `npm i immutability-helper`
+
 then install boostrap v4
-`npm install bootstrap@4.0.0-alpha.6`
+
+### `npm install bootstrap@4.0.0-alpha.6`
 
--- src/TableLine.js
+++ src/TableLine.js
@@ -42,13 +42,13 @@ class TableLine extends Component {
                 <td>{singleRow.status > 0 ? (<label>{singleRow.job}</label>) : (<input type="text" className="form-control" name="job" defaultValue ={singleRow.job || null} onChange={this.handleChange}></input>)}</td>
                 <td>{singleRow.status < 0 ? (
                     <div>
-                    <input type="button" className="btn btn-primary" value="Submit" onClick={() => this.submitForm(index)} />
+                    <input type="button" className="btn btn-primary" value="Save" onClick={() => this.submitForm(index)} />
                     &nbsp;
                     <input type="button" className="btn btn-default" value="Cancel" onClick={() => updateStatus(index, 1)} />
                     </div>) : (
 
                     <div>
-                        <button className="btn btn-primary" onClick={() => this._updateStatus(index, -1)}>Edit</button>
+                        <button className="btn btn-warning" onClick={() => this._updateStatus(index, -1)}>Edit</button>
                          &nbsp;
                         <button className="btn btn-danger" onClick={() => removeCharacter(index)}>Delete</button>
                     </div>
